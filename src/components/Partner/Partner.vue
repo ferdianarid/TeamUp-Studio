32893a3499b4c5a926d5f248b8619fc63ad15593
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-10-28 11:56:23 +0700

    partner

--- src/components/Partner/Partner.vue
+++ src/components/Partner/Partner.vue
@@ -1,6 +1,22 @@
 <template>
-       <div class="partner w-full h-48 bg-yellow-100">
-              <h1>We Have Collaborated With</h1>
+<div class="content-story w-full h-auto pt-16 pb-16 grid grid-cols-2">
+              <div>
+                     <h1 class="font-medium pt-16 leading-snug text-white text-left text-5xl">We Have Collaborated With</h1>
+              </div>
+              <div class="grid grid-cols-2">
+                     <div>
+                            <img src="../../assets/images/spotify.png" alt="">
+                     </div>
+                     <div>
+                            <img src="../../assets/images/youtube.png" alt="">
+                     </div>
+                     <div>
+                            <img src="../../assets/images/spotify.png" alt="">
+                     </div>
+                     <div>
+                            <img src="../../assets/images/youtube.png" alt="">
+                     </div>
+              </div>
        </div>
 </template>
 
