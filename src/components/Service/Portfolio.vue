5f9a49378425ba2ca6af5fafba63d1e8aaa0c04f
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-10-29 10:50:24 +0700

    portfolio component

--- src/components/Service/Portfolio.vue
+++ src/components/Service/Portfolio.vue
@@ -1,6 +1,25 @@
 <template>
-       <div class="w-full h-56 bg-yellow-300 pt-32">
-              <h1>Portfolio Component</h1>
+       <div class="portfolio w-full h-auto pt-16 pb-16">
+              <h1 class="pt-4 leading-snug text-white text-left text-5xl font-extrabold pb-7">Our Works</h1>
+              <div class="flex text-white uppercase">
+                     <div class="card-image w-1/2 mr-4">
+                            <img src="../../assets/images/laptop.png" alt="">
+                            <p class="category pt-5">Dashboard, Desktop, UI Design</p>
+                            <p class="capitalize font-extrabold text-2xl">Dashboard Accounting</p>
+                     </div>
+                     <div class="w-1/2" style="display: flex;">
+                            <div class="card-image w-1/2 mr-1">
+                                   <img src="../../assets/images/skilled.png" alt="">
+                            <p class="category pt-5">Mobile, UI Design, IOS</p>
+                            <p class="capitalize font-extrabold text-2xl">Skilled UI Design</p>
+                            </div>
+                            <div class="card-image w-1/2">
+                                   <img src="../../assets/images/phone.png" alt="">
+                            <p class="category pt-5">Mobile, UI Design, IOS</p>
+                            <p class="capitalize font-extrabold text-2xl">Greenbarn Apps</p>
+                            </div>
+                     </div>
+              </div>
        </div>
 </template>
 
