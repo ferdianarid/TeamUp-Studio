d7593901432368edf3a45368e589471bb47d3582
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-11-01 23:26:48 +0700

    responsive

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,13 +1,13 @@
 <template>
        <div class="footer flex justify-between w-full h-auto bg-yellow-400 pt-16 text-white">
-              <div class="w-1/2">
+              <div class="footer-left w-1/2">
                      <h1 class="leading-snug text-5xl text-white font-extrabold">TeamUp Studio</h1>
                      <p class="paragraph leading-8 pt-4">
                             Teamup Studio is a creative digital studio who focused on UI/UX Design, Illustration, Branding and Graphic Design. 
                      </p>
                      <p class="pt-7 font-extrabold">Jl. Puntodewo No.07, Krajan, Wedomartani, Sleman, Yogyakarta, Indonesia</p>
               </div>
-              <div class="w-1/2ml-auto pt-12">
+              <div class="footer-right w-1/2ml-auto pt-12">
                      <p>Email Address</p>
                      <p class="font-extrabold">hello@teamupstudio.id</p>
                      <p class="pt-7">Instagram</p>
