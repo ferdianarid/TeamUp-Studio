55d73c55c7e3a837f725e13d54aba4383fbe62f4
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-11-01 18:47:20 +0700

    parter responsive

--- src/components/Partner/Partner.vue
+++ src/components/Partner/Partner.vue
@@ -1,19 +1,17 @@
 <template>
-<div class="content-story collaborate w-full h-auto pt-16 pb-16 grid grid-cols-2">
-              <div>
-                     <h1 class="font-extrabold pt-16 leading-snug text-white text-left text-5xl">We Have Collaborated With</h1>
-              </div>
-              <div class="grid grid-cols-2">
-                     <div>
+<div class="partner w-full h-auto pt-16 pb-16 grid grid-cols-2">
+              <h1 class="font-extrabold pt-16 leading-snug text-white text-left text-5xl">We Have Collaborated With</h1>
+              <div class="colm grid grid-cols-2">
+                     <div class="sponsor">
                             <img src="../../assets/images/spotify.png" alt="">
                      </div>
-                     <div>
+                     <div class="sponsor">
                             <img src="../../assets/images/youtube.png" alt="">
                      </div>
-                     <div>
+                     <div class="sponsor">
                             <img src="../../assets/images/spotify.png" alt="">
                      </div>
-                     <div>
+                     <div class="sponsor">
                             <img src="../../assets/images/youtube.png" alt="">
                      </div>
               </div>
