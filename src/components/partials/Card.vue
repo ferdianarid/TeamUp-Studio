2cf31a21d915f30c1c9d134762ff5ecd8a010787
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-11-01 12:34:27 +0700

    added axios

---
+++ src/components/partials/Card.vue
@@ -0,0 +1,20 @@
+<template>
+       <div>
+              {{ data }}
+       </div>
+</template>
+
+<script>
+export default {
+       data() {
+              return {
+                     data
+              }
+       },
+       mounted() {
+              axios.get("https://jsonplaceholder.typicode.com/users").then(response => this.data = response)
+       }
+}
+</script>
+
+<style></style>
