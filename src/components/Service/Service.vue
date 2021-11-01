f096415669df6b5faacc94ffdc0e45a67acfa7af
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-11-01 19:42:59 +0700

    service responsive

--- src/components/Service/Service.vue
+++ src/components/Service/Service.vue
@@ -1,6 +1,6 @@
  <template>
         <div class="servicess w-full pt-12 pb-12 bg-secondary-color h-auto">
-               <div class="grid grid-cols-3 gap-7 ml-28 mr-28">
+               <div class="servicesss grid grid-cols-3 gap-7 ml-28 mr-28">
                       <div class="">
                              <h1 class="pt-4 leading-snug text-white text-left text-5xl font-extrabold">We can help you with</h1>
                       </div>
