c6a0dca7a362f4c843b06f0148d8ff207c682bcc
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-11-01 14:26:25 +0700

    styling update

--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -2,6 +2,7 @@
        <div class="card w-1/3 bg-white h-auto rounded-lg p-4 text-left shadow-xl">
               <h1 class="text-2xl font-bold pb-3">Lorem, ipsum.</h1>
               <p>Lorem ipsum dolor, sit amet consectetur</p>
+              {{ data }}
        </div>
 </template>
 
@@ -13,7 +14,7 @@ export default {
               }
        },
        mounted() {
-              axios.get("https://jsonplaceholder.typicode.com/users").then(response => this.data = response)
+              axios.get("https://jsonplaceholder.typicode.com/users").then(response => (this.data = response))
        }
 }
 </script>
