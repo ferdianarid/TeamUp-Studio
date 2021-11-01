91c1e61bb020536164df065f65b84144b2b15dc6
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-11-01 22:06:53 +0700

    portfolio responsive

--- src/components/Service/Portfolio.vue
+++ src/components/Service/Portfolio.vue
@@ -1,13 +1,13 @@
 <template>
        <div class="portfolio w-full h-auto pt-16 pb-16">
-              <h1 class="pt-4 leading-snug text-white text-left text-5xl font-extrabold pb-7">Our Works</h1>
-              <div class="flex text-white uppercase">
-                     <div class="card-image w-1/2 mr-4">
+              <h1 class="portfolio-heading pt-4 leading-snug text-white text-left text-5xl font-extrabold pb-7">Our Works</h1>
+              <div class="portfolios flex text-white uppercase">
+                     <div class="single card-image w-1/2 mr-4">
                             <img src="../../assets/images/laptop.png" alt="">
                             <p class="category pt-5">Dashboard, Desktop, UI Design</p>
                             <p class="capitalize font-extrabold text-2xl">Dashboard Accounting</p>
                      </div>
-                     <div class="w-1/2" style="display: flex;">
+                     <div class="duoCard w-1/2" style="display: flex;">
                             <div class="card-image w-1/2 mr-1">
                                    <img src="../../assets/images/skilled.png" alt="">
                             <p class="category pt-5">Mobile, UI Design, IOS</p>
