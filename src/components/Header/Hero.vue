c1265c218ec70622c65a01be6f41d69f6c5eb6d0
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-11-02 14:32:46 +0700

    header animate

--- src/components/Header/Hero.vue
+++ src/components/Header/Hero.vue
@@ -18,8 +18,8 @@ export default {
        <div class="hero container w-full">
               <!-- Title and Subtitle -->
               <div class="heading w-2/3">
-                     <h1 class="title-headings leading-snug pt-14 text-5xl text-white font-extrabold">{{ title }}</h1>
-                     <p class="leading-normal pt-5 paragraph text-white font-medium">{{ subtitle }}</p>
+                     <h1 data-aos="fade-right" data-aos-easing="ease-out" data-aos-duration="1000" class="title-headings leading-snug pt-14 text-5xl text-white font-extrabold">{{ title }}</h1>
+                     <p data-aos="fade-right" data-aos-easing="ease-out" data-aos-duration="1000" data-aos-delay="800" class="leading-normal pt-5 paragraph text-white font-medium">{{ subtitle }}</p>
                      <!-- Button Contact Us -->
                      <Button />
               </div>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import { createApp } from 'vue'
 // import app
 import App from './App.vue'
 
+// Import AOS
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+// Store to variable
+const app = createApp(App)
+
+// Initialize AOS Package
+app.use(AOS.init())
+
 // import tailwind css dist
 import "../src/tailwind.css"
 
@@ -10,4 +20,4 @@ import "../src/tailwind.css"
 import "../src/assets/stylesheets/index.css"
 
 // mount app
-createApp(App).mount('#app')
+app.mount('#app')
